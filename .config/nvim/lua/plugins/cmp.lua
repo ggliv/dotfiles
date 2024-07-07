855a9155c27eec8f48a9efb9682227a9d2973743
Gabe Livengood <gabe@ggliv.com>
2024-07-07 11:39:59 -0400

    nvim: remove friendly-snippets
    I really don't think I've ever actually wanted to use any of these

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -10,9 +10,6 @@ return {
 
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets',
   },
 
   config = function()
