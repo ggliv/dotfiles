7aafaaaaa4845d32db60796b1024128bf3d05a9d
Gabe Livengood <gabe@ggliv.com>
2024-07-11 01:08:52 -0400

    meta: move config files to more visible places

--- install.sh
+++ install.sh
@@ -6,17 +6,21 @@ BASE_DIR="$(cd $(dirname "$0"); pwd)"
 cd "$BASE_DIR"
 
 # Add contents of .config
-for e in .config/*; do
-    if [ -d ~/"$e" ]; then
-        printf "Delete existing config directory ~/%s? " "$e"
-        read c
-        [ "$c" = "y" ] && rm -rf ~/"$e"
+for e in config/*; do
+  de=."$e"
+  if [ -e ~/"$de" ]; then
+    printf "Replace existing config directory ~/%s? " "$de"
+    read c
+    if [ "$c" = "y" ]; then
+      mv ~/"$de" ~/"$de".bak
+      echo "Placed backup at ~/$de.bak"
     fi
-    ln -sfib "$BASE_DIR"/"$e" ~/"$(dirname "$e")"
+  fi
+  ln -sfib "$BASE_DIR"/"$e" ~/"$de"
 done
 
 # Add top-level files prefixed with .
-for e in \.*; do
-    [ -d "$e" ] && continue
-    ln -sfib "$BASE_DIR"/"$e" ~/"$e"
+for e in home/\.*; do
+  [ -d "$e" ] && continue
+  ln -sfib "$BASE_DIR"/"$e" ~/"$(basename -- $e)"
 done
