bb0f6f1ce230d599879de6756e7f1a835f1b5c2f
Gabe Livengood <gabe@ggliv.com>
2024-07-12 22:35:49 -0400

    nvim: don't notify about plugin updates

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -24,8 +24,8 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- auto-check for plugin updates, notify once every 24 hours max (in seconds)
+  checker = { enabled = true, notify = false },
   -- use unicode instead of nerd font symbols
   ui = {
     icons = {
